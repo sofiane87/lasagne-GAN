807ddc9b6b4b9baa11c6e6e2477d1b73070a77af
sofiane87 <sofiane-mahiou@hotmail.fr>
2017-08-15 14:51:39 +0100

    quick commit

--- dcgan/dcgan_celeba3.py
+++ dcgan/dcgan_celeba3.py
@@ -2,7 +2,7 @@ from __future__ import print_function
 
 from keras.datasets import mnist
 from keras.layers import Input, Dense, Reshape, Flatten, Dropout
-from keras.layers import BatchNormalization, Activation, ZeroPadding2D
+from keras.layers import BatchNormalization, Activation, ZeroPadding2D, Lambda
 from keras.layers.advanced_activations import LeakyReLU
 from keras.layers.convolutional import UpSampling2D, Conv2D
 from keras.models import Sequential, Model
